a39ab8566d093576a9c6687453e38196a7556c85
1bertom15-spec <1bertom15@gmail.com>
2025-11-13 05:10:22 -0100

    Initial commit~

--- 3D Final project app/api/util/db.js
+++
@@ -1,9 +0,0 @@
-import { sql } from '@vercel/postgres';
-import { v4 as uuidv4 } from 'uuid';
-
-export const getClient = () => {
-    return sql;
-};
-
-// (Nota: A função setupDatabase() que estava aqui não é necessária 
-// se fizermos a configuração manual, como recomendado.)
